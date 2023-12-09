9495c7ccf948ababea55de25dbf33b865f286d25
Emir <emirkan.ulker9@gmail.com>
2023-12-09 18:58:21 +0300

    v

---
+++ ornek3/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ornek3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            pictureBox1.BackColor = Color.Aqua;
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("www.google.com");
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("www.onedio.com");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            linkLabel4.LinkBehavior = LinkBehavior.HoverUnderline;
+            linkLabel1.LinkBehavior = LinkBehavior.HoverUnderline;
+            linkLabel5.LinkBehavior = LinkBehavior.HoverUnderline;
+        }
+
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("Calc.exe");
+        }
+
+        private void linkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("MsPaint.exe");
+        }
+    }
+}
